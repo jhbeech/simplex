e54f98b423d9d91c1b6aa0d43b84965264def349
Joey Beech <joeybeech@Joeys-MacBook-Air.local>
2023-07-27 19:07:19 +0800

    branch and bound incorrect implementation. Would work for binary variables so saving it for later

--- simplex_solver.py
+++ simplex_solver.py
@@ -1,12 +1,16 @@
 import numpy as np
 import numpy.typing as npt
 from typing import Optional
+import math
 
 
 class Node:
     def __init__(self, vars: list[int], vals: list[float]):
         self.vars = vars
         self.vals = vals
+
+    def copy(self):
+        return Node(self.vars[:], self.vals[:])
 
     def return_next_node(self, var: int, val: float):
         new_node = self
@@ -33,8 +37,9 @@ class LpProblem:
         b: npt.NDArray[np.float64],
         c: npt.NDArray[np.float64],
         obj: Optional[float] = None,
-        basis: Optional[list[int]] = None,
-        basis_vals: Optional[list[float]] = None,
+        basis: Optional[dict[int, Optional[float]]] = None,
+        # basis: Optional[list[int]] = None,
+        # basis_vals: Optional[list[float]] = None,
     ):
         self.A = A
         self.b = b
@@ -58,21 +63,32 @@ class LpProblem:
         if branch_node:
             print("As well as branch constraints:")
             for var, val in zip(branch_node.vars, branch_node.vals):
-                print(f"x_{var} == val")
+                print(f"x_{var} == {val}")
 
-    def get_initial_basis(self, branch_node: Optional[Node] = None):
-        ## maybe should be called set?
+    def set_initial_basis(self):
+        """simply sets slack variables to be basic, ie non slack variables are set to 0"""
         A = self.A
-        excluded_vars = branch_node.vars if branch_node else []
-        ## optimize?
-        self.basis = [
-            i for i in range(A.shape[0], A.shape[1]) if i not in excluded_vars
+        self.basis = {
+            i: None for i in range(A.shape[1] - A.shape[0], A.shape[1])
+        }
+
+    def get_most_fractional_var(self) -> int:
+        """selects basic variables are returns the one with the most fractional value (fractional part closest to 0.5)"""
+        basis = self.basis
+        if not basis:
+            return None
+        non_slack_basis_vars = [
+            var for var in basis.keys() if var < self.A.shape[0]
         ]
+        non_slack_basis_dist_fractional = {
+            k: basis[k] % 1 for k in non_slack_basis_vars
+        }
+        return min(
+            non_slack_basis_dist_fractional,
+            key=non_slack_basis_dist_fractional.get,
+        )
 
-    def get_most_fractional_var(self) -> Node:
-        idx = np.argmin(self.basis_vals)
-        return Node([self.basis[idx]], [self.basis_vals[idx]])
-
+    ## includes vals fixed by currents branch in obj
     def primal_simplex(
         self, max_iterations: int, branch_node: Optional[Node] = None
     ):
@@ -84,75 +100,95 @@ class LpProblem:
             vals_fixed_by_branch = np.zeros(A.shape[1])
             vals_fixed_by_branch[branch_node.vars] = branch_node.vals
             b = b - A @ vals_fixed_by_branch
+            obj_min = c @ vals_fixed_by_branch
+            if b.min() < 0:
+                print("infeasible")
+                self.obj = None
+                self.basis = None
+                return -999
 
-        basis: list[int] = self.basis
-        non_basis = get_non_basic(basis, A.shape[0], branch_node)
+            if A.shape[0] == len(branch_node.vars):
+                print(
+                    f"Branch has fixed all variables, no need for simplex. Obj {obj_min}"
+                )
+                # print(f"{get_basis_values(A, b)}")
+
+                self.obj = obj_min
+                self.basis = None
+                return obj_min
+        else:
+            obj_min = 0
+
+        if b.min() < 0:
+            print("infeasible")
+            self.obj = None
+            self.basis = None
+            return -999
+        basis_vars: list[int] = list(self.basis.keys())
+        non_basis_vars = get_non_basic(basis_vars, A.shape[1], branch_node)
 
         for it in range(max_iterations):
-            A_b = A[:, basis]
-            A_n = A[:, non_basis]
-            c_b = c[basis]
-            c_n = c[non_basis]
+            A_b = A[:, basis_vars]
+            A_n = A[:, non_basis_vars]
+            c_b = c[basis_vars]
+            c_n = c[non_basis_vars]
 
             print(
-                f"iteration: {it}. Objective: {get_objective_value(c_b, A_b, b)}. Basis: {basis}. Vals {get_basis_values(A_b, b)}"  ## remove this for speed
+                f"iteration: {it}. Objective: {get_objective_value(c_b, A_b, b)}. Basis: {basis_vars}. Vals {get_basis_values(A_b, b)}"  ## remove this for speed
             )
 
+            # print(f"{basis_vars=}")
             reduced_costs = get_reduced_costs(A_b, A_n, c_b, c_n)
-            entering_var = get_entering_var_primal(reduced_costs, non_basis)
+            entering_var = get_entering_var_primal(
+                reduced_costs, non_basis_vars
+            )
             leaving_var = get_leaving_var_primal(
-                basis, A, A_b, b, entering_var
+                basis_vars, A, A_b, b, entering_var
             )
 
             if entering_var is not None:
-                basis.remove(leaving_var)
-                basis.append(entering_var)
-                non_basis.remove(entering_var)
-                non_basis.append(leaving_var)
+                basis_vars.remove(leaving_var)
+                basis_vars.append(entering_var)
+                non_basis_vars.remove(entering_var)
+                non_basis_vars.append(leaving_var)
             else:
                 print(
-                    f"Optimal solution found: {get_objective_value(c_b, A_b, b)}"
+                    f"Optimal solution found: {get_objective_value(c_b, A_b, b) + obj_min}"
                 )
                 break
 
-        self.basis = basis
-        self.basis_vals = get_basis_values(A_b, b)
-        self.obj = get_objective_value(c_b, A_b, b)
+        self.basis = {
+            b: v for b, v in zip(basis_vars, get_basis_values(A_b, b))
+        }
+        self.obj = get_objective_value(c_b, A_b, b) + obj_min
 
         return self.obj
 
-    def get_rounded_objective(self) -> float:
+    def get_rounded_objective(
+        self, branch_node: Optional[Node] = None
+    ) -> float:
         """if simplex has already been solved, simply rounds all fractional values for x down and finds objective"""
-        return np.floor(self.basis_vals) @ self.c[self.basis]
-
-
-def get_branch_constraints(
-    A: npt.NDArray[np.float64],
-    b: npt.NDArray[np.float64],
-    c: npt.NDArray[np.float64],
-    branch_node: Node,
-) -> tuple[
-    npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64]
-]:
-    """returns the A,b such that the values from the node/branch are set."""
-    new_b = np.append(b, branch_node.vals)
-    A_extra_rows = np.array(
-        [
-            [1 if j == i else 0 for j in range(A.shape[1])]
-            for i in branch_node.vars
-        ]
-    )
-
-    A_extra_columns = np.zeros(
-        (A.shape[0] + len(branch_node.vars), len(branch_node.vars))
-    )
-
-    new_A = np.append(
-        np.append(A, A_extra_rows, axis=0), A_extra_columns, axis=1
-    )
-    new_c = np.append(c, np.zeros(len(branch_node.vars)))
-
-    return (new_A, new_b, new_c)
+        # for var, val in self.basis.items():
+        #     print(f"{var=}", f"c_coff={self.c[var]}", f"val={math.floor(val)}")
+        if self.basis:
+            return sum(
+                self.c[var] * math.floor(val)
+                for var, val in self.basis.items()
+            ) + (
+                sum(
+                    self.c[var] * val
+                    for var, val in zip(branch_node.vars, branch_node.vals)
+                )
+                if branch_node
+                else 0
+            )
+        else:
+            if self.A.shape[0] == len(branch_node.vars):
+                vals_fixed_by_branch = np.zeros(self.A.shape[1])
+                vals_fixed_by_branch[branch_node.vars] = branch_node.vals
+                return self.c @ vals_fixed_by_branch
+            else:
+                return -999
 
 
 def branch_and_bound(
@@ -161,66 +197,95 @@ def branch_and_bound(
     c,
     gap: float,
     max_nodes_to_search: int,
-    gomory_max_cuts: Optional[int] = None,
+    max_iterations_simplex: int,
+    # gomory_max_cuts: Optional[int] = None,
 ):
     """branch and bound - assumes all variables are binary lol"""
 
     prob = LpProblem(A, b, c)
-    prob.get_initial_basis()
-    prob.primal_simplex(10)
+    prob.set_initial_basis()
+    prob.primal_simplex(max_iterations_simplex)
     # prob.gomory_cut_algorithm(gomory_max_cuts)
     upper_bound = prob.obj
     ## round to ints and find obj
     lower_bound = prob.get_rounded_objective()
+    print(f"best lower/integral bound {lower_bound}")
+    print()
+    if upper_bound == lower_bound:
+        print(f"First sol is integral {lower_bound}")
+        return lower_bound
 
     ## first in first out
-    queue = [prob.get_most_fractional_var()]
+    queue = [Node([prob.get_most_fractional_var()], [-1])]
 
     best_int_node = queue
+    nodes_searched = 0
     while queue:
-        branch_node = queue.pop(0)
-        for branch_node_val in [0, 1]:
+        print("queue: ", [i.vars for i in queue])
+        branch_node_no_val_assigned = queue.pop(0)
+        print("---------- new branch var -----------")
+        for branch_node_val in [0, 1, 2, 3]:
+            branch_node = branch_node_no_val_assigned.copy()
+            print(
+                f"nodes searched {nodes_searched}. branching: let x_{branch_node.vars[-1]}={branch_node_val}."
+            )
+            ################# branch node needs to start fres in each iteration here.....................................
             ## branch_node.set_leaf_val(branch_node_val)
             branch_node.vals[-1] = branch_node_val
             ## we are searching this node now
             ## if it is worth searching we will branch into it and add its children
             ## otherwise we dont want it (its parent is already popped)
-
             ## add constraint forcing branch_var = branch_val
-            # A_branch, b_branch, c_branch = get_branch_constraints(
-            #     A, b, c, branch_node
-            # )
-            # prob = LpProblem(A_branch, b_branch, c_branch)
-            prob = LpProblem(A, b, c)
-            prob.get_initial_basis(branch_node)
-            print(f"{prob.basis=}")
-            prob.print_problem()
 
-            new_relaxation = prob.primal_simplex(10, branch_node)
+            prob = LpProblem(A, b, c)
+            prob.set_initial_basis()
+            prob.print_problem(branch_node)
+
+            new_relaxation = prob.primal_simplex(
+                max_iterations_simplex, branch_node
+            )
+
             ## probably in fact faster to use current basis and add constraint and do dual simplex??
             ## would work if doing depth first tree as we are iteratively adding constraints
             ## in breadth first we need to re-initialise lp problem.
-            new_integral = prob.get_rounded_objective()
+            new_integral = prob.get_rounded_objective(branch_node)
+            print(f"Integral sol {new_integral}")
 
             nodes_searched += 1
             if new_relaxation < lower_bound:
                 print("prune :)")
-                ## by not adding new_node to queue, this branch is gone (i think)
-                pass
+                ## by not adding a child node to queue (for each possible value of branch_node), this branch is gone
+                continue
 
             else:
-                ## keep the node for further searching
-                queue.append(branch_node)
+                ## get child node for further searching
+                next_branch_var = prob.get_most_fractional_var()
+                print(f"{next_branch_var=}")
+                if next_branch_var is not None:
+                    queue.append(
+                        branch_node.return_next_node(next_branch_var, -1)
+                    )
+                    print(
+                        f"couldn't prune - searching next most fractional var {queue[-1].vars[-1]}"
+                    )
+                else:
+                    print("nothing else to search thorugh")
 
             if new_integral > lower_bound:
                 lower_bound = new_integral
                 best_int_node = branch_node_val
 
-            if (upper_bound - lower_bound < gap) or (
-                nodes_searched > max_nodes_to_search
-            ):
-                print("bounds close enough, go with best integral sol")
-                break
+            if upper_bound - lower_bound < gap:
+                print(
+                    f"bounds close enough, go with best integral sol {lower_bound}"
+                )
+                return lower_bound
+            if nodes_searched > max_nodes_to_search:
+                print(f"exceeded max iterations - best so far: {lower_bound}")
+                return lower_bound
+            print()
+
+    print(f"best sol {lower_bound}")
 
     return lower_bound
 
@@ -246,16 +311,18 @@ def get_reduced_costs(
 
 
 def get_non_basic(
-    basis: list[int], num_columns: int, branch_node: Optional[Node] = None
+    basis_vars: list[int], num_columns: int, branch_node: Optional[Node] = None
 ) -> list[int]:
     ignore_ids = branch_node.vars if branch_node else []
     return [
-        i for i in range(num_columns) if i not in basis and i not in ignore_ids
+        i
+        for i in range(num_columns)
+        if i not in basis_vars and i not in ignore_ids
     ]
 
 
 def get_leaving_var_primal(
-    basis: list[int],
+    basis_vars: list[int],
     A: npt.NDArray[np.float64],
     A_b: npt.NDArray[np.float64],
     b: npt.NDArray[np.float64],
@@ -263,7 +330,7 @@ def get_leaving_var_primal(
 ):
     np.seterr(divide="ignore")
     return (
-        basis[
+        basis_vars[
             np.argmin(
                 np.linalg.inv(A_b)
                 @ b
@@ -288,32 +355,64 @@ def get_basis_values(
 
 
 def main():
-    # A = np.array([[1.05, 0.95, 1.5, 0], [2, 1, 0, 1]])
-    # b = np.array([12, 16])
-    # c = np.array([40, 30, 0, 0])
+    A = np.array([[1, 7, 1, 0], [1, 0, 0, 1]])
+    b = np.array([17.5, 3.5])
+    c = np.array([1, 10, 0, 0])
+    # prob = LpProblem(A, b, c)
+    # prob.print_problem()
+    # prob.set_initial_basis()
+    # prob.primal_simplex(10000)
+    # print(f"integral solution {prob.get_rounded_objective()}")
 
-    A = np.array([[2, 1, 1, 0], [1, 0, 0, 1]])
-    b = np.array([10, 15])
-    c = np.array([3, 5, 0, 0])
-    prob = LpProblem(A, b, c)
-    prob.print_problem()
-    prob.get_initial_basis()
-    prob.primal_simplex(10)
-    print("----------------------")
+    # branch_node = Node([0], [2])
+    # prob.set_initial_basis()
+    # prob.print_problem(branch_node)
+    # prob.primal_simplex(10, branch_node)
+    # print(f"integral solution {prob.get_rounded_objective(branch_node)}")
 
-    branch_node = Node([0], [3])
-    prob = LpProblem(A, b, c)
-    prob.print_problem(branch_node)
-    prob.get_initial_basis(branch_node)
-    prob.primal_simplex(10, branch_node)
-    # print(prob.get_rounded_objective())
-    # branch_and_bound(A, b, c, 0.01, 10)
+    # print("-" * 25)
+    branch_and_bound(A, b, c, 0.01, 1000, 1000)
 
     return None
 
 
+import time
+
 if __name__ == "__main__":
+    tic = time.monotonic()
     main()
+    print(time.monotonic())
+
+## better to make A smaller rather than bigger
+# def get_branch_constraints(
+#     A: npt.NDArray[np.float64],
+#     b: npt.NDArray[np.float64],
+#     c: npt.NDArray[np.float64],
+#     branch_node: Node,
+# ) -> tuple[
+#     npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64]
+# ]:
+#     """returns the A,b such that the values from the node/branch are set."""
+#     new_b = np.append(b, branch_node.vals)
+#     A_extra_rows = np.array(
+#         [
+#             [1 if j == i else 0 for j in range(A.shape[1])]
+#             for i in branch_node.vars
+#         ]
+#     )
+
+#     A_extra_columns = np.zeros(
+#         (A.shape[0] + len(branch_node.vars), len(branch_node.vars))
+#     )
+
+#     new_A = np.append(
+#         np.append(A, A_extra_rows, axis=0), A_extra_columns, axis=1
+#     )
+#     new_c = np.append(c, np.zeros(len(branch_node.vars)))
+
+#     return (new_A, new_b, new_c)
+
+
 """
 problem: A,b,c
 basis = initialBasis(A,b)
